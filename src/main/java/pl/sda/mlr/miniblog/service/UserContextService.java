48bc579cc1c9cd0934b58793720fef4fb319a3fb
LAPTOP-36SKAH06\marla <marlenalukomska@wp.pl>
2019-03-04 14:32:32 +0100

    Methods: hasAnyRole(String... roleNames) and hasAllRoles(String... roleNames) in UserContextService.

--- src/main/java/pl/sda/mlr/miniblog/service/UserContextService.java
+++ src/main/java/pl/sda/mlr/miniblog/service/UserContextService.java
@@ -6,14 +6,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserContextService {
 
-    public String getLoggedAs(){
+    public String getLoggedAs() {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();     //ten obiekt jest zawze, niezależnie czy użytkownik jest zalogowany, czy nie; instancją autentykacji jest użytkownik anonimowy(niezalogowany) i użytkownik zalogowany
 
-        if(authentication instanceof AnonymousAuthenticationToken){ //jeśli jest instancją, to mamy niezalogowanego użytkownika
+        if (authentication instanceof AnonymousAuthenticationToken) { //jeśli jest instancją, to mamy niezalogowanego użytkownika
             return null;
         }
 
@@ -21,14 +24,49 @@ public class UserContextService {
 
     }
 
-    //TODO: hasRole (String... roleNames)
-    public boolean hasRole(String roleName){
+    public boolean hasAnyRole(String... roleNames) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication instanceof AnonymousAuthenticationToken){
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return false;
+        } else {
+            List<Boolean> booleans = getBooleans(authentication, roleNames);
+            if (booleans.contains(true)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasAllRoles(String... roleNames) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return false;
+        } else {
+            List<Boolean> booleans = getBooleans(authentication, roleNames);
+            if (booleans.contains(false)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Boolean> getBooleans(Authentication authentication, String[] roleNames) {
+        List<Boolean> booleans = new ArrayList<>();
+        for (String roleName : roleNames) {
+            booleans.add(authentication.getAuthorities().stream()
+                    .map(a -> (a.getAuthority()))
+                    .anyMatch(s -> s.equals(roleName)));
+        }
+        return booleans;
+    }
+
+    public boolean hasRole(String roleName) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
             return false;
         }
         return authentication.getAuthorities().stream()
-                .map(a->a.getAuthority())
-                .anyMatch(s->s.equals(roleName));
+                .map(a -> a.getAuthority())
+                .anyMatch(s -> s.equals(roleName));
     }
 }
